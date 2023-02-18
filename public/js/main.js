c9712822ff452069af0cd3ad59dec3837d2706e9
erickbmelo <erick.melo@sorocaba.sp.gov.br>
2023-02-17 23:44:04 -0300

    Remove placar event.preventDefault() parent().remove()

--- public/js/main.js
+++ public/js/main.js
@@ -66,14 +66,21 @@ function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Erick";
     var numPalavras = $("#contador-palavras").text();
+    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
 
     var linha = "<tr>"+
                     "<td>"+ usuario + "</td>"+
                     "<td>"+ numPalavras + "</td>"+
+                    "<td>"+ botaoRemover + "</td>"+
                 "</tr>";
 
     corpoTabela.prepend(linha);
 }
+
+$(".botao-remover").click(function(event){
+    window.event.preventDefault();
+    $(this).parent().parent().remove();
+});
 
 function reiniciaJogo(){
     campo.attr("disabled", false);
