a0f6e6c893553ec62e3fcb5fc8822fabef952d6a
erickbmelo <erick.melo@sorocaba.sp.gov.br>
2023-02-18 00:22:10 -0300

    Adicionando eventos nas linhas do placar

--- public/js/main.js
+++ public/js/main.js
@@ -66,21 +66,38 @@ function inserePlacar(){
     var corpoTabela = $(".placar").find("tbody");
     var usuario = "Erick";
     var numPalavras = $("#contador-palavras").text();
-    var botaoRemover = "<a href='#'><i class='small material-icons'>delete</i></a>"
 
-    var linha = "<tr>"+
-                    "<td>"+ usuario + "</td>"+
-                    "<td>"+ numPalavras + "</td>"+
-                    "<td>"+ botaoRemover + "</td>"+
-                "</tr>";
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.prepend(linha);
 }
 
-$(".botao-remover").click(function(event){
-    window.event.preventDefault();
-    $(this).parent().parent().remove();
-});
+function novaLinha(usuario, palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href", "#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removeLinha(){
+        window.event.preventDefault();
+        $(this).parent().parent().remove();
+}
+
 
 function reiniciaJogo(){
     campo.attr("disabled", false);
