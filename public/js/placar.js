efbdd493c11272231515a543a3366449de61e486
erickbmelo <erick.melo@sorocaba.sp.gov.br>
2023-02-20 16:43:44 -0300

    Animando o scroll

--- public/js/placar.js
+++ public/js/placar.js
@@ -9,6 +9,15 @@ function inserePlacar() {
     linha.find(".botao-remover").click(removeLinha);
 
     corpoTabela.append(linha);
+    $(".placar").slideDown(500);
+    scrollPlacar();
+}
+
+function scrollPlacar(){
+    var posicaoPlacar = $(".placar").offset().top;
+    $("body").animate({
+        scrollTop: posicaoPlacar+"px"
+    }, 1000);
 }
 
 function novaLinha(usuario, palavras) {
