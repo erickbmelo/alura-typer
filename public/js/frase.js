7cce98e83c3231ac2f4b2bb2013a1d156c2ef013
Erick de Brito Melo <erick.melo@prefeitura.local>
2023-03-16 13:29:42 -0300

    Buscando dados com get

--- public/js/frase.js
+++ public/js/frase.js
@@ -1,4 +1,5 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria() {
     $("#spinner").show();
@@ -21,4 +22,34 @@ function trocaFraseAleatoria(data) {
     frase.text(data[numeroAleatorio].texto);
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase() {
+
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+
+    var dados = {id : fraseId}; //criacao do objeto JS que guarda a id
+
+    //passando objeto como segundo parâmetro
+    $.get("http://localhost:3000/frases", dados, trocaFrase)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+        },2000);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data) {
+
+    console.log(data);
+
+    var frase = $(".frase");
+    frase.text(data.texto); //cuidado, texto com "o" no final 
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
